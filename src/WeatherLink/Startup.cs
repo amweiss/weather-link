6b41189dc60d19c25c76512051804e4fd9b806e5
Adam Weiss <adam.weiss@perkinelmer.com>
2016-11-04 11:17:01 -0400

    Setup json configs for debug and azure

--- src/WeatherLink/Startup.cs
+++ src/WeatherLink/Startup.cs
@@ -17,8 +17,10 @@ namespace WeatherLink
 		{
 			var builder = new ConfigurationBuilder()
 				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) //TODO: Find better way to handle hosting options and debug paths
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
 				.AddJsonFile("src/WeatherLink/appsettings.json", optional: true, reloadOnChange: true)
-				.AddJsonFile($"src/WeatherLinkappsettings.{env.EnvironmentName}.json", optional: true)
+				.AddJsonFile($"src/WeatherLink/appsettings.{env.EnvironmentName}.json", optional: true)
 				.AddEnvironmentVariables();
 			Configuration = builder.Build();
 		}
